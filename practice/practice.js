3a2366efe9b1b83ec944d629862a1cc0cffc476a
letherius Miller <letherius1@gmail.com>
2022-10-16 13:41:21 -0500

    homework

--- practice/practice.js
+++ practice/practice.js
@@ -1,6 +1,9 @@
+let listTasks = [];// empty array
 //constructor
-function Task(description,priority)
-this.priority=priority;
+function Task(description,priority){
+    this.title=description;// this is the problem ***
+    this.priority=priority;
+}
 
 //global variable
 let inputPractice = document.getElementById('txtPractice'); //getting the html element
@@ -8,15 +11,16 @@ let selectPriority = document.getElementById('selPriority');
 let task="";
 
 function register(){
-    let newTask = new Task();
-    display();
+    let newTask = new Task(inputPractice.value, selectPriority.value);
+    listTasks.push(newTask);
+    display(newTask);
     inputPractice.value=""; //clearing the input
     inputPractice.className="error";//adding the css class
 }
 
-function display(){
+function display(aTask){
     let tasksSection = document.getElementById("tasks");//getting the HTML element
     //task += "<p class='task'>" + inputPractice.value + "</p>"; //create the task html element
-    task+=`<li class="task">${inputPractice.value} - ${selPriority.value} <button>🗑️</button></li>`; 
+    task+=`<li class="task">${aTask.title} --- ${aTask.priority} <button>🗑️</button></li>`; 
     tasksSection.innerHTML=task;//injecting the task into the html
 }
